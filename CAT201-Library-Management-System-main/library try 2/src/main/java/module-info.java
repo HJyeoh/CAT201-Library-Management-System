2d167790371dcb8a12ea01ea62536c7157447c45
HJyeoh <147971774+HJyeoh@users.noreply.github.com>
2024-11-22 14:40:32 +0800

    Delete CAT201-Library-Management-System-main/library try 2 directory

--- CAT201-Library-Management-System-main/library try 2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.codewithmosh.librarytry2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.codewithmosh.librarytry2 to javafx.fxml;
-    exports com.codewithmosh.librarytry2;
-}
